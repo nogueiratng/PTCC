a2c23a2c8bc8e6d40bc75b7b9696789c7569e273
THIAGO NOGUEIRA <nogueira.tng@gmail.com>
2026-04-19 15:55:17 -0300

    alteração SQLite para PostgreSQL

--- app/settings.py
+++ app/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tcc_edu',        # Nome do banco 
+        'USER': 'postgres',       # Usuário padrão 
+        'PASSWORD': 'postgres', 
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
